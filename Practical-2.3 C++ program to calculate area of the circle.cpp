6a9a7e0a39bdb87addebcf60d7d8a3f93764138a
shrutipanchal19 <129736617+shrutipanchal19@users.noreply.github.com>
2023-04-12 19:19:34 +0530

    Add files via upload

---
+++ Practical-2.3 C++ program to calculate area of the circle.cpp
@@ -0,0 +1,20 @@
+//C++ program to calculate area of the circle
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+float radius, area_circle;
+
+// take radius as input
+cout << "Enter the radius of circle: ";
+cin >> radius;
+
+area_circle = 3.14 * radius * radius;
+
+cout << "Area of circle: " << area_circle << endl;
+
+return 0;
+}
